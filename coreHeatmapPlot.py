097c78d622d1a46337f86b48c74893d5e4fd6321
JFeghalyHypertec <jfeghaly@hypertec.com>
2025-07-16 08:40:16 -0400

    fixed table

--- coreHeatmapPlot.py
+++ coreHeatmapPlot.py
@@ -100,8 +100,8 @@ def build_summary_table(df, core_df, file_path, plate=None, occt_version=None):
     cpu_str = f"{cpu_package:.2f} °C" if not np.isnan(cpu_package) else "NA"
     avg_str = f"{avg_core_tmp:.2f} °C"
 
-    columns = ["Plate", "OCCT Version", CPU_PACKAGE, "Overall Average Core", WATER_FLOW]  # Move Plate and OCCT to the front
-    values  = [wf_str, cpu_str, avg_str, plate, occt_version]
+    columns = ["Plate", "OCCT Version", CPU_PACKAGE, "Overall Average Core", WATER_FLOW]
+    values = [plate, occt_version, cpu_str, avg_str, wf_str]
 
     return pd.DataFrame([values], columns=columns)
 
